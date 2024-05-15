bc0ab6f618bf21b8673311a60674f1cb6a4d1dd5
422145328@qq.com <422145328@qq.com>
2024-05-15 10:06:59 +0800

    add twenty

---
+++ cvm-twenty/build_docker.sh
@@ -0,0 +1,21 @@
+# get shell path
+SOURCE="$0"
+while [ -h "$SOURCE"  ]; do
+    DIR="$( cd -P "$( dirname "$SOURCE"  )" && pwd  )"
+    SOURCE="$(readlink "$SOURCE")"
+    [[ $SOURCE != /*  ]] && SOURCE="$DIR/$SOURCE"
+done
+DIR="$( cd -P "$( dirname "$SOURCE"  )" && pwd  )"
+cd $DIR/
+
+cp ./Dockerfile ./twenty
+cd ./twenty
+
+tag=`date "+%Y-%m-%d-%H-%M"`
+
+chmod +x run.sh 
+
+docker build -t wetee/twenty:$tag .
+docker push wetee/twenty:$tag
+
+# docker run -e ser_port=7880 -e udp_port=7881 -e tcp_port=7882 -e node_ip=127.0.0.1 -p 7880:7880 -p 7881:7881 -p 7882:7882/udp wetee/cvm-livekit-server:$tag /run.sh
