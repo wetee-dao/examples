f510b0f88e020f63adb56a8ea6e4ff16ed1462de
422145328@qq.com <422145328@qq.com>
2024-05-18 15:06:40 +0800

    update proxy docker

---
+++ cvm-livekit/script/build_docker_ui.sh
@@ -0,0 +1,17 @@
+# get shell path
+SOURCE="$0"
+while [ -h "$SOURCE"  ]; do
+    DIR="$( cd -P "$( dirname "$SOURCE"  )" && pwd  )"
+    SOURCE="$(readlink "$SOURCE")"
+    [[ $SOURCE != /*  ]] && SOURCE="$DIR/$SOURCE"
+done
+DIR="$( cd -P "$( dirname "$SOURCE"  )" && pwd  )"
+cd $DIR/../
+
+tag=`date "+%Y-%m-%d-%H-%M"`
+
+
+docker build -f Dockerfile.ui -t wetee/cvm-livekit-ui:$tag .
+docker push wetee/cvm-livekit-ui:$tag
+
+# docker run -e ser_port=7880 -e udp_port=7881 -e tcp_port=7882 -e node_ip=127.0.0.1 -p 7880:7880 -p 7881:7881 -p 7882:7882/udp wetee/cvm-livekit-server:$tag /run.sh
--- cvm-ssl-proxy/run.sh
+++ cvm-ssl-proxy/run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 /env "$KEY"
-/ssl-proxy -cert cert.pem -key key.pem -from 0.0.0.0:4430 -to $TO
+/ssl-proxy -cert cert.pem -key key.pem -from 0.0.0.0:$PORT -to $TO
